be2cfe1b458a03554e13554717911d7b0914178c
R1ght <o.gorelikow@gmail.com>
2017-02-17 11:58:35 +0300

    Initial Commit

---
+++ Copyright/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Copyright
+//
+//  Created by Олег on 17.02.17.
+//  Copyright © 2017 Gorelikov production. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
